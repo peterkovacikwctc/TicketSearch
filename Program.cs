78f6c38705a3b698089eb08c2c33ba6442348d2b
Peter Kovacik <pkovacik2@my.wctc.edu>
2020-09-27 19:22:26 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ticketing_system_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ticketing System with Classes and Objects");
+        }
+    }
+}
